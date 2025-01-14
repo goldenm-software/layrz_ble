536eebd4cae031a28856321e80fc1391a5ecefa5
panchove <fpradove@gmail.com>
2025-01-14 09:04:40 -0500

    Init Windows Support Fix Method Calls

--- windows/include/layrz_ble_plugin.cpp
+++ windows/include/layrz_ble_plugin.cpp
@@ -1,34 +1,94 @@
-#pragma once
-#include <flutter/method_channel.h>
-#include <flutter/plugin_registrar_windows.h>
-#include <flutter/standard_method_codec.h>
-#include <windows.h>
-#include <string>
-#include <map>
-#include <vector>
+#include "layrz_ble_plugin.h"
 
-class LayrzBlePlugin : public flutter::Plugin {
-public:
-	static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+void LayrzBlePlugin::CheckCapabilities (std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to check capabilities
+  result->Success (flutter::EncodableValue (true));
+}
 
-	LayrzBlePlugin();
+void
+LayrzBlePlugin::StartScan(
+  const flutter::MethodCall<flutter::EncodableValue> &method_call, 
+  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result )
+{
+  // Implement the logic to start scanning
+  result->Success (flutter::EncodableValue (true));
+}
 
-	virtual ~LayrzBlePlugin();
+void
+LayrzBlePlugin::StopScan(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+{
+  // Implement the logic to stop scanning
+  result->Success (flutter::EncodableValue (true));
+}
 
-private:
-	void HandleMethodCall(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+void LayrzBlePlugin::Connect(
+  const flutter::MethodCall<flutter::EncodableValue> &method_call,
+  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to connect to a device
+  result->Success (flutter::EncodableValue (true));
+}
 
-	void CheckCapabilities(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void StartScan(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void StopScan(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void Connect(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void Disconnect(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void DiscoverServices(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void SetMtu(std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void WriteCharacteristic(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void ReadCharacteristic(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void StartNotify(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-	void StopNotify(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+void
+LayrzBlePlugin::Disconnect (std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to disconnect from a device
+  result->Success (flutter::EncodableValue (true));
+}
 
-	void Log(const std::string &message);
-};
+void
+LayrzBlePlugin::DiscoverServices (std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to discover services
+  result->Success (flutter::EncodableValue (true));
+}
+
+void
+LayrzBlePlugin::SetMtu (std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to set MTU
+  result->Success (flutter::EncodableValue (true));
+}
+
+void
+LayrzBlePlugin::WriteCharacteristic (
+  const flutter::MethodCall<flutter::EncodableValue> &method_call,
+  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to write a characteristic
+  result->Success (flutter::EncodableValue (true));
+}
+
+void
+LayrzBlePlugin::ReadCharacteristic (
+  const flutter::MethodCall<flutter::EncodableValue> &method_call,
+  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to read a characteristic
+  result->Success (flutter::EncodableValue (true));
+}
+
+void
+LayrzBlePlugin::StartNotify (
+  const flutter::MethodCall<flutter::EncodableValue> &method_call,
+  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to start notifications
+  result->Success (flutter::EncodableValue (true));
+}
+
+void
+LayrzBlePlugin::StopNotify (
+  const flutter::MethodCall<flutter::EncodableValue> &method_call,
+  std::unique_ptr<flutter::MethodResult<flutter::EncodableValue> > result)
+{
+  // Implement the logic to stop notifications
+  result->Success (flutter::EncodableValue (true));
+}
+
+void
+LayrzBlePlugin::Log (const std::string &message)
+{
+  OutputDebugStringA (("LayrzBlePlugin/Windows: " + message + "\n").c_str ());
+}
